e347b9c5d981f586c2555145cbb7c3f02e513c02
Patrick Ward <patricksjward@gmail.com>
2020-04-19 12:00:00 -0700

    feat: add machine code draw function

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,84 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+//While Nothing is pressed
+(NOTPRESSEDLOOP)
+@KBD
+D=M
+@R0
+M=D
+@NOTPRESSEDLOOP
+D;JEQ
+
+@8191
+D=A
+@i
+M=D
+
+@SCREEN
+D=A
+@currentIndex
+M=D
+
+//make screen black
+@SCREEN
+D=A
+M=-1
+(PAINTINGSCREEN)
+@currentIndex
+M=M+1
+A=M
+M=-1
+
+//loop back
+@i
+M=M-1
+D=M
+@PAINTINGSCREEN
+D;JNE
+
+//WhileSomething Is pressed
+(PRESSEDLOOP)
+@KBD
+D=M
+@PRESSEDLOOP
+D;JNE
+
+//PAINT WHITE
+@8191
+D=A
+@i
+M=D
+
+@SCREEN
+D=A
+@currentIndex
+M=D
+
+//make screen white
+@SCREEN
+D=A
+M=0
+(PAINTINGSCREENWHITE)
+@currentIndex
+M=M+1
+A=M
+M=0
+
+//loop back
+@i
+M=M-1
+D=M
+@PAINTINGSCREENWHITE
+D;JNE
+
+@NOTPRESSEDLOOP
+D;JMP
+
+
+//END
+(END)
+@END
+D;JMP
